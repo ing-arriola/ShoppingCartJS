3a8ce0c21eac688d538367131e2a69f63d35ba59
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-01 04:37:13 -0600

    Added the function to delete courses from the cart

--- js/app.js
+++ js/app.js
@@ -7,24 +7,30 @@ const courseList = document.querySelector('#lista-carrito tbody')
 loadListeners()
 
 function loadListeners(){
+    //Listerner for the courses list element in the DOM
     courses.addEventListener('click',buyCourse)
+
+    cart.addEventListener('click',deleteCourse)
 }
 //Functions
-
+//This function determines with delegation if the clicked element is a "a" markup with the class "agregar carrito"
 function buyCourse(e){
     e.preventDefault()
     //Delegation to add course on cart
-    if(e.target.classList.contains('agregar-carrito')){
-        const course=e.target.parentElement.parentElement
-        //Send data of the course for reading
+    if(e.target.classList.contains('agregar-carrito')){//If this condition is True then
+        const course=e.target.parentElement.parentElement//Save the HTML of the element
+        //Send data of the course for reading.
         readCourseData(course)
     }
     
 }
 
 //Read course data
+//We need to read course selected information in order to send the information to the cart..
+//When the client see the cart, it will contains the selected elements from the course list
 const readCourseData=(course)=>{
     console.log(course)
+    //Object that contains all the info of a selected course
     const courseData ={
         image: course.querySelector('img').src,
         title: course.querySelector('h4').textContent,
@@ -32,11 +38,13 @@ const readCourseData=(course)=>{
         id: course.querySelector('a').getAttribute('data-id')
     }
     
-    insertIntoCart(courseData)
+    insertIntoCart(courseData)//Send information to be inserted in the cart
 }
 
 const insertIntoCart=(curso)=>{
-    const row=document.createElement('tr')
+    const row=document.createElement('tr')//Create a table row element for each course add at the cart
+    //Here is the hmtl structure inside the tr, a detail of the selected course with image, tittle, price and...
+    //... a "X" to delete the course of the cart
     row.innerHTML=`
     <td> 
         <img src="${curso.image}">
@@ -48,9 +56,21 @@ const insertIntoCart=(curso)=>{
         ${curso.price}
     </td>
     <td> 
-        <a href="#" class"borrar-curso" data-id="${curso.image}">"X"</a>
+        <a href="#" class="borrar-curso" data-id="${curso.image}">X</a>
     </td>
     `
-    courseList.appendChild(row)
+    courseList.appendChild(row)//Finished the structure of the tr, it is added to the courseList in the cart
+
+}
+//This function deletes a course from the cart when the element "X" is clicked 
+function deleteCourse(e) {
+    e.preventDefault()//Prevent the default action of the anchor tag
+    //Delegation... we must validate that the clicked element has the classlist "borrar-curso"... because it means that the user wants to delete 
+    //a course from the cart, after that the targert ups two parentElement to achieve the "tr".. as you can see in insertIntoCart function
+    //The first parent of the anchor tag is the td... but we need to delete all the information of the selected course and the elementent
+    //that contains that data is the "tr"
+    if(e.target.classList.contains('borrar-curso')){
+        e.target.parentElement.parentElement.remove()
+    }
 
 }
